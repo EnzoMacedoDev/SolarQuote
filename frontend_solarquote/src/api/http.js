81b5e8e47d64d8abd9e8e4e9ae696126d99f9ae8
enzozcm <enzo.macedo.prg@gmail.com>
2026-09-03 18:25:47 -0300

    Evita JWT no login

--- frontend_solarquote/src/api/http.js
+++ frontend_solarquote/src/api/http.js
@@ -1,23 +1,19 @@
 import axios from 'axios';
 
 const http = axios.create({
-
   baseURL: 'https://gds-solar-backend.onrender.com',
-
 });
 
 http.interceptors.request.use((config) => {
-
   const token = localStorage.getItem('token');
 
-  if (token) {
+  const isAuthRoute = config.url?.startsWith('/auth/');
 
+  if (token && !isAuthRoute) {
     config.headers.Authorization = `Bearer ${token}`;
-
   }
 
   return config;
-
 });
 
 export default http;
